1a4f2d5d72204a71fd4994e4cfed7314acabf378
Andrey <andrewboyko_91@mail.ru>
2022-02-10 15:56:44 +0300

    Add HomeworkSeven

---
+++ HomeworkSeven/src/main/java/ApplicationGlobalState.java
@@ -0,0 +1,30 @@
+public final class ApplicationGlobalState {
+
+    private static ApplicationGlobalState INSTANCE;
+    private String selectedCity = null;
+    private final String API_KEY = "Gn0nki2QVRKGe1HpPyvbT4FTEIC2x2Dy";
+
+    private ApplicationGlobalState() {
+    }
+
+    // Непотокобезопасный код для упрощения
+    public static ApplicationGlobalState getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ApplicationGlobalState();
+        }
+
+        return INSTANCE;
+    }
+
+    public String getSelectedCity() {
+        return selectedCity;
+    }
+
+    public void setSelectedCity(String selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public String getApiKey() {
+        return this.API_KEY;
+    }
+}
